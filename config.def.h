c8a57d87cbfe1dba658d2a3ce015b4231a37fd5d
NoTArZuZ <arbuztvshka@yandex.ru>
2024-06-16 22:43:30 +0500

    Changed colors

--- config.def.h
+++ config.def.h
@@ -26,15 +26,15 @@ static const unsigned int alphas[][3]      = {
 };
 
 static
-char *colors[][2] = {
+char *colors[SchemeLast][2] = {
 	/*               fg         bg       */
 	[SchemeNorm] = { "#cccccc", "#282c34" },
 	[SchemeSel]  = { "#1c1f24", "#c678dd" },
-	[SchemeOut]  = { "#000000", "#51afef" },
-	[SchemeBorder] = { "#000000", "#c678dd" },
-	[SchemeMid]  = { "#d7d7d7", "#1c1f24" },
 	[SchemeSelHighlight]  = { "#98be65", "#000000" },
 	[SchemeNormHighlight] = { "#98be65", "#000000" },
+	[SchemeOut]  = { "#000000", "#51afef" },
+	[SchemeMid]  = { "#d7d7d7", "#1c1f24" },
+	[SchemeBorder] = { "#000000", "#c678dd" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
